b309f381f5632f2d7abe675324e2892bc875985a
Nevfel Emre <nevfel.cicek@gmail.com>
2022-10-06 14:44:48 +0300

    variables, print, types

---
+++ PythonGiris.py
@@ -0,0 +1,30 @@
+print("Merhaba")
+print("Dünya")
+
+if 1 < 2:
+    print("true")
+
+a = 5
+b = None
+print (b)
+c = 1.5
+d = -80349534759834759834759834
+print(a, b, c, d)
+print(a, b, c, d, sep = ", ")
+
+print("aynı", end = " ")
+print("satırdayım")
+
+a, b, c = 1, 3, 7
+
+print(a, b, c)
+
+h = [1, 2, 3]
+g = {a: "asd", b: "kurmançi"}
+f = ("constant", "array")
+
+print("a: ", type(a))
+
+print(type(h))
+print(type(g))
+print(type(f))
