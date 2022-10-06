ea6c5aa547e980176d364712041208049c3c791f
Nevfel Emre <nevfel.cicek@gmail.com>
2022-10-06 15:38:03 +0300

    metinler

---
+++ metinler.py
@@ -0,0 +1,23 @@
+a = "jsjsjsjs"
+b = """saopsmadp
+
+asğdöğasdöv
+
+şasşldölşasd
+"""
+print(a, b)
+print(a[2])
+
+a = "balıkesir"
+if "kes" in a and a == "balıkesir":
+    print("kes var")
+
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+
+print(a.__len__())
+print(len(a))
+
+print('İstanbul\'un dağları')
+print('İstanbul\\un dağları')
