3239c06c92da10a93bb3475f3ac8f218fa00a3a8
Nevfel Emre <nevfel.cicek@gmail.com>
2022-10-13 14:06:18 +0300

    algoritma sorusu

---
+++ algoritmaSorusu.py
@@ -0,0 +1,6 @@
+"""
+1- Klavyeden girilen 5 tane sayıyı bir listeye atarak bunların karelerinden 20 çıktığında
+ortaya çıkan sonuca göre sıralayan ve listeyi buna göre yazdıran programı yazınız.
+"""
+
+
